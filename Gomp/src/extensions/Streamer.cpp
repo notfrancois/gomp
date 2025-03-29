7d49890b5e3f25ccec965747fbb90eec9e54a70c
François López <francois@piscesmetadata.com>
2025-03-29 15:25:20 -0500

    feat: Enhance dynamic object creation in Streamer
    - Added default parameters for stream distance, draw distance, area ID, and priority in the `streamer_createDynamicObject` function.
    - Introduced new Docker build and entrypoint scripts for streamlined containerized builds.
    - Created a Dockerfile for setting up the build environment with necessary dependencies.

--- Gomp/src/extensions/Streamer.cpp
+++ Gomp/src/extensions/Streamer.cpp
@@ -12,7 +12,13 @@ GOMPONENT_EXPORT int streamer_createDynamicObject(int modelid, const float x, co
 {
     IOmpStreamerComponent* streamer = Gomp::Get()->getStreamer();
 
-    if (const std::shared_ptr<streamer::IObject> object = streamer->createDynamicObject(modelid, Vector3(x, y, z), Vector3(rx, ry, rz), worldid, interiorid, playerid)) {
+    // Valores predeterminados para los parámetros faltantes
+    const float defaultStreamDistance = 300.0f;
+    const float defaultDrawDistance = 300.0f;
+    const int defaultAreaId = -1;
+    const int defaultPriority = 0;
+
+    if (const std::shared_ptr<streamer::IObject> object = streamer->createDynamicObject(modelid, Vector3(x, y, z), Vector3(rx, ry, rz), worldid, interiorid, playerid, defaultStreamDistance, defaultDrawDistance, defaultAreaId, defaultPriority)) {
         return object->getID();
     }
 
